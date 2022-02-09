6b39eb98e2eeeb46a85ba055171ae841d80041cc
Karmester <ptrarmand10@gmail.com>
2022-02-09 15:41:57 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import http from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState("");
+	const [color, setColor] = useState("");
+	const [cats, setCats] = useState([]);
+
+	const add = async () => {
+		await http.post("http://localhost:3000/api/cats", {
+			name,
+			color,
+		});
+		load();
+	};
+	const load = async () => {
+		const response = await http.get("http://localhost:3000/api/cats");
+		setCats(response.data);
+	};
+	const del = async (id) => {
+		await http.delete(`http://localhost:3000/api/cats/${id}`);
+		load();
+	};
+
+	useEffect(() => {
+		load();
+	}, []);
+
+	return (
+		<div className="App">
+			<input
+				type="text"
+				placeholder="Cat name"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<input
+				type="color"
+				value={color}
+				onChange={(e) => setColor(e.target.value)}
+			/>
+			<button onClick={() => add()}>Add</button>
+			{cats.map((cat) => (
+				<div key={cat.id} style={{ backgroundColor: cat.color }}>
+					{cat.name}
+					<button onClick={() => del(cat.id)}>Delete</button>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
